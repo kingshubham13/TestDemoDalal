0db215f62a0f4bfee5597f5b0aa2f7c8cfbb1025
kingshubham13 <112744265+kingshubham13@users.noreply.github.com>
2022-11-03 10:06:40 +0530

    TestDemoDalal project
    hii guys,
    
    I have created TestDemoDalal project
    so clone it and start work.

---
+++ TestDemoDalal/src/main/java/com/testBase/TestBase.java
@@ -0,0 +1,52 @@
+package com.testBase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+
+	public static WebDriver driver;
+	@BeforeMethod
+	public void setup()
+	{
+		String br ="chrome";
+		if (br.equalsIgnoreCase("chrome"))
+		{
+		  WebDriverManager.chromedriver().setup();
+	      driver = new ChromeDriver();
+		}
+		else if (br.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if (br.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver =new EdgeDriver();
+		}
+		else
+		{
+			System.out.println("Please provide correct browser");
+		}
+		driver.get("https://www.apps.dalalstreet.ai/login");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
